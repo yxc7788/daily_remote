a6c3de0bcea27823d8269d0ad6b1a230cc8124d9
杨晓川 <yangxc27652@hundsun.com>
2020-09-08 14:51:37 +0800

    随机数生成方法新增

--- src/daily/RandomGenerator.java
+++ src/daily/RandomGenerator.java
@@ -13,7 +13,7 @@ public class RandomGenerator {
         // 输出0到1之间得数，默认的double类型
         for (int i = 0;i < 5;i++) {
             double num = Math.random();
-            System.out.println(num);
+            System.out.println("随机数方法1 : "+ num);
         }
 
         //   注意 Math.random() 这个方法返回是double类型，注意取值的范围[0,1)（能够取到零值，注意除零异常），
@@ -22,7 +22,15 @@ public class RandomGenerator {
             Random random = new Random();
             // 该方法的作用是生成一个随机的int值，该值介于[0,n)的区间，也就是0到n之间的随机int值，包含0而不包含n
             int num = random.nextInt(10);
-            System.out.println(num);
+            System.out.println("随机数方法2 : "+ num);
+        }
+
+        // 这种方法也可以返回一个[m,n)之间的随机数整数
+        for (int i = 0;i < 5;i++) {
+            int m = 5;
+            int n = 10;
+            int  num = (int)(Math.random()*(n-m)+m);
+            System.out.println("随机数方法3 : "+ num);
         }
 
     }
