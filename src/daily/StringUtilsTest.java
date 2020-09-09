e4fea1de6dc5a9bd0e57f57af9939612cab0c05c
yangxc <yangxc27652@hundsun.com>
2020-09-09 22:29:17 +0800

    String工具类提交

---
+++ src/daily/StringUtilsTest.java
@@ -0,0 +1,17 @@
+package daily;
+
+/**
+ * @author yangxc27652
+ * @date 2020/9/9
+ * @description  JAVA中操作字符String常用的方法
+ */
+public class StringUtilsTest {
+
+    public static void main(String[] args) {
+
+        // 字符串替换
+        String a = "aabb";
+        a.replace("a","aa");
+        System.out.println(a);
+    }
+}
