ad78a7836ac157a026ff6125ebb0377cb75e13bb
杨晓川 <yangxc27652@hundsun.com>
2020-09-15 16:39:44 +0800

    option方法测试提交

--- spring_demo/src/main/java/com/example/demo/OptionTest.java
+++ spring_demo/src/main/java/com/example/demo/OptionTest.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 /**
  * @author yangxc27652
  * @date 2020/7/28
- * @description  option测试
+ * @description  option测试   https://www.runoob.com/java/java8-optional-class.html
  */
 public class OptionTest {
     public static void main(String[] args) {
@@ -23,5 +23,24 @@ public class OptionTest {
         Object o1 = "";
         Object o2 = null;
         System.out.println(ObjectUtils.nullSafeEquals(o1,o2));
+
+        System.out.println("----------");
+
+        // orElse()    https://www.jianshu.com/p/790f7c185d3e
+        //当optional值不存在时，调用orElse()返回orElse()的参数，如果optional的值存在时返回optional的值
+        String nullValue = null;
+        String optional = Optional.ofNullable(nullValue).orElse("Su");
+        System.out.println(optional);
+
+        //orElseGet()
+        //当optional值不存在时，调用orElseGet()中接口调用的返回值，如果optional的值存在时返回optional的值
+        String optionalGet = Optional.ofNullable(nullValue).orElseGet(() -> "Xiao");
+        System.out.println(optionalGet);
+
+        String nonNullOptional = Optional.ofNullable("Susan").orElse("Su");
+        System.out.println(nonNullOptional);
+
+        String nonNullOptionalGet = Optional.ofNullable("Molly").orElseGet(() -> "Xiao");
+        System.out.println(nonNullOptionalGet);
     }
 }
