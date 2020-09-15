81dbfa0072a17ff568f1afe607c9356436e45d66
杨晓川 <yangxc27652@hundsun.com>
2020-09-15 19:05:33 +0800

    springboot启动类测试

--- spring_demo/src/main/java/com/example/demo/DemoApplication.java
+++ spring_demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,41 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * The type Demo application.
+ *
+ */
+@RestController
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 @SpringBootApplication
 public class DemoApplication {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    /**
+     * Test string.
+     * @author
+     * @return the string
+     * @date
+     */
+    @RequestMapping("/test")
+    @ResponseBody()
+    public String test() {
+        return "haha";
+    }
 }
