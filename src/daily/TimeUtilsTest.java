6df4abbd41473bf965ecde104bc09826f9623857
杨晓川 <yangxc27652@hundsun.com>
2020-09-15 14:07:54 +0800

    时间类测试代码提交

--- src/daily/TimeUtilsTest.java
+++ src/daily/TimeUtilsTest.java
@@ -1,5 +1,8 @@
 package daily;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author yangxc27652
  * @date 2020/9/10
@@ -8,6 +11,23 @@ package daily;
 public class TimeUtilsTest {
 
     public static void main(String[] args) {
+
+        // 新建一个date对象
+        Date date = new Date();
+        System.out.println(date);   //  Tue Sep 15 13:54:27 CST 2020
+
+        // 取当前机器日期
+        SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
+        String machineDate = df.format(date);
+        System.out.println("当前机器日期为:" + machineDate);
+
+        // 取当前机器时间
+        SimpleDateFormat df1 = new SimpleDateFormat("HHmmss");
+        String machineTime = df1.format(date);
+        System.out.println("当前机器时间为:" + machineTime);
+
+        Long startTs = System.currentTimeMillis(); // 当前时间戳
+        System.out.println("当前时间戳为:"+startTs);
 
     }
 }
