215e7e087954424be0339b032bcf326c41812ada
杨晓川 <yangxc27652@hundsun.com>
2020-09-15 17:00:24 +0800

    枚举类测试

---
+++ src/daily/EnumTest.java
@@ -0,0 +1,79 @@
+package daily;
+
+/**
+ * The type Enum test.
+ *
+ * @author yangxc27652
+ * @date 2020 /9/15
+ * @description 遍历枚举测试
+ */
+public class EnumTest {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        // 遍历一个枚举时候注意用  枚举对象.values
+        for (SearchCategoryEnum s : SearchCategoryEnum.values()) {
+            System.out.println(s.getValue());
+        }
+        System.out.println("------------");
+        System.out.println(SearchCategoryEnum.ALL);
+        System.out.println(SearchCategoryEnum.ALL.getValue());
+    }
+}
+
+/**
+ * The enum Search category.
+ */
+enum SearchCategoryEnum {
+
+    /**
+     * All search category.
+     */
+    ALL("综合查询"),
+    /**
+     * News search category.
+     */
+    NEWS("新闻"),
+    /**
+     * Taxpayer search category.
+     */
+    TAXPAYER("纳税人"),
+    /**
+     * Quiz search category.
+     */
+    QUIZ("知识问答"),
+    /**
+     * Policy law search category.
+     */
+    POLICY_LAW("政策法规"),
+    /**
+     * Doc search category.
+     */
+    DOC("涉税文档"),
+    /**
+     * Pic search category.
+     */
+    PIC("图片"),
+    /**
+     * Baike search category.
+     */
+    BAIKE("税务百科");
+
+    private String value;
+
+    SearchCategoryEnum(String value) {
+        this.value = value;
+    }
+
+    /**
+     * Get value string.
+     *
+     * @return the string
+     */
+    public String getValue(){
+        return value;
+    }
+}
