ce9754eb8b79c0312f2e0842340fe8f3423d8fa9
杨晓川 <yangxc27652@hundsun.com>
2020-12-02 13:27:06 +0800

    剑指offer算法

--- src/algorithm/All.java
+++ src/algorithm/All.java
@@ -844,7 +844,39 @@ public class All {
             this.random = null;
         }
     }
-    public RandomListNode Clone(RandomListNode pHead) {
+
+    /**
+     * 方法1
+     */
+    public RandomListNode copyRandomList(RandomListNode head) {
+        HashMap<RandomListNode,RandomListNode> map = new HashMap<>(); //创建HashMap集合
+        RandomListNode cur=head;
+        //复制结点值
+        while(cur!=null){
+            //存储put:<key,value1>
+            map.put(cur,new RandomListNode(cur.val)); //顺序遍历，存储老结点和新结点(先存储新创建的结点值)
+            cur=cur.next;
+        }
+        //复制结点指向
+        cur = head;
+        while(cur!=null){
+            //得到get:<key>.value2,3
+            map.get(cur).next = map.get(cur.next); //新结点next指向同旧结点的next指向
+            map.get(cur).random = map.get(cur.random); //新结点random指向同旧结点的random指向
+            cur = cur.next;
+        }
+        //返回复制的链表
+        return map.get(head);
+
+    }
+
+
+    /**
+     * 方法2
+     * @param pHead
+     * @return
+     */
+    public RandomListNode Clone2(RandomListNode pHead) {
         if(pHead == null) {
             return null;
         }
